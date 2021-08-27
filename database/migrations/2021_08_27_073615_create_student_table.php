26eb36156f9f87849a6fe7f84b700c76d36b8333
deoagent <deoagent@gmail.com>
2021-08-27 16:07:11 +0800

    #migrate

---
+++ database/migrations/2021_08_27_073615_create_student_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 120);
+            $table->string("email", 80);
+            $table->integer("age")->unsigned();
+            $table->string("phone", 20);
+            $table->enum("gender", ["male", "female"]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student');
+    }
+}
